ea5b526b54bcd0eca45be9114aa3c8ef1a01c839
KnapMartin <martin_knap@yahoo.com>
2025-02-17 16:28:50 +0100

    add write, read, reset, print functionality

--- MB85RC512T.c
+++ MB85RC512T.c
@@ -7,6 +7,11 @@
 
 #include "MB85RC512T.h"
 
+#include <string.h>
+#if MB85RC512T_PRINT == 1
+#include <stdio.h>
+#endif
+
 MB85RC512T_State MB85RC512T_init(MB85RC512T *device, I2C_HandleTypeDef *hi2c, const uint8_t address)
 {
 	device->m_address = address << 1;
@@ -25,16 +30,120 @@ MB85RC512T_State MB85RC512T_deinit(MB85RC512T *device)
 	return MB85RC512T_OK;
 }
 
-MB85RC512T_State MB85RC512T_write8(MB85RC512T *device, const uint8_t data)
+MB85RC512T_State MB85RC512T_write(MB85RC512T *device, const uint32_t address, const uint8_t *data, const size_t len)
 {
+	if (!device->m_init) return MB85RC512T_ERROR_INIT;
+	if (address + len - 1 > MB85RC512T_MAX_ADDRESS) return MB85RC512T_ERROR_ADDRESS;
+
+	device->m_data_tx[0] = (uint8_t)(address >> 8);
+	device->m_data_tx[1] = (uint8_t)(address & 0xFF);
+
+	for (uint32_t ctr = 0; ctr < len; ++ctr)
+	{
+		device->m_data_tx[ctr + 2] = data[ctr];
+	}
+
+	if (HAL_I2C_Master_Transmit(device->m_hi2c, device->m_address, device->m_data_tx, len + 2, MB85RC512T_TIMEOUT) != HAL_OK)
+	{
+		return MB85RC512T_ERROR_TX;
+	}
+
 	return MB85RC512T_OK;
 }
 
-MB85RC512T_State MB85RC512T_read8(MB85RC512T *device, uint8_t *data)
+MB85RC512T_State MB85RC512T_read(MB85RC512T *device, const uint32_t address, uint8_t *data, const size_t len)
 {
+	if (!device->m_init) return MB85RC512T_ERROR_INIT;
+	if (address + len - 1 > MB85RC512T_MAX_ADDRESS) return MB85RC512T_ERROR_ADDRESS;
+
+	device->m_data_tx[0] = (uint8_t)(address >> 8);
+	device->m_data_tx[1] = (uint8_t)(address & 0xFF);
+
+	if (HAL_I2C_Master_Transmit(device->m_hi2c, device->m_address, device->m_data_tx, 2, MB85RC512T_TIMEOUT) != HAL_OK)
+	{
+		return MB85RC512T_ERROR_TX;
+	}
+
+	if (HAL_I2C_Master_Receive(device->m_hi2c, device->m_address, device->m_data_rx, len, MB85RC512T_TIMEOUT) != HAL_OK)
+	{
+		return MB85RC512T_ERROR_RX;
+	}
+
+	memcpy(data, device->m_data_rx, len);
+
 	return MB85RC512T_OK;
 }
 
-#if MB85RC512T_TEST == 1
+MB85RC512T_State MB85RC512T_reset(MB85RC512T *device, const uint8_t value)
+{
+	if (!device->m_init) return MB85RC512T_ERROR_INIT;
 
+	uint32_t writeLen = MB85RC512T_WRITE_LEN;
+
+	for (uint32_t byteCtr = 0; byteCtr < writeLen; ++byteCtr)
+	{
+		device->m_data_tx[byteCtr + 2] = value;
+	}
+
+	for (uint32_t address = 0; address < MB85RC512T_MAX_ADDRESS; address += writeLen)
+	{
+		device->m_data_tx[0] = (uint8_t)(address >> 8);
+		device->m_data_tx[1] = (uint8_t)(address & 0xFF);
+
+		if (HAL_I2C_Master_Transmit(device->m_hi2c, device->m_address, device->m_data_tx, writeLen + 2, MB85RC512T_TIMEOUT) != HAL_OK)
+		{
+			return MB85RC512T_ERROR_TX;
+		}
+	}
+
+	return MB85RC512T_OK;
+}
+
+#if MB85RC512T_PRINT == 1
+MB85RC512T_State MB85RC512T_print(MB85RC512T *device, UART_HandleTypeDef *huart)
+{
+	if (!device->m_init) return MB85RC512T_ERROR_INIT;
+
+	uint32_t readLen = 8;
+	uint8_t printBuff[16];
+
+	for (uint32_t address = 0; address < MB85RC512T_MAX_ADDRESS; address += readLen)
+	{
+		device->m_data_tx[0] = (uint8_t)(address >> 8);
+		device->m_data_tx[1] = (uint8_t)(address & 0xFF);
+
+		if (HAL_I2C_Master_Transmit(device->m_hi2c, device->m_address, device->m_data_tx, 2, MB85RC512T_TIMEOUT) != HAL_OK)
+		{
+			return MB85RC512T_ERROR_TX;
+		}
+
+		if (HAL_I2C_Master_Receive(device->m_hi2c, device->m_address, device->m_data_rx, readLen, MB85RC512T_TIMEOUT) != HAL_OK)
+		{
+			return MB85RC512T_ERROR_RX;
+		}
+
+		sprintf((char*)printBuff, "%04X:", (uint16_t)address);
+		if (HAL_UART_Transmit(huart, printBuff, strlen((char*)printBuff), MB85RC512T_TIMEOUT_UART) != HAL_OK)
+		{
+			return MB85RC512T_ERROR_UART;
+		}
+
+		for (uint32_t byteCtr = 0; byteCtr < readLen; ++byteCtr)
+		{
+			sprintf((char*)printBuff, " %02X", device->m_data_rx[byteCtr]);
+			if (HAL_UART_Transmit(huart, printBuff, strlen((char*)printBuff), MB85RC512T_TIMEOUT_UART) != HAL_OK)
+			{
+				return MB85RC512T_ERROR_UART;
+			}
+		}
+
+		sprintf((char*)printBuff, "\r\n");
+		if (HAL_UART_Transmit(huart, printBuff, strlen((char*)printBuff), MB85RC512T_TIMEOUT_UART) != HAL_OK)
+		{
+			return MB85RC512T_ERROR_UART;
+		}
+	}
+
+	return MB85RC512T_OK;
+}
 #endif
--- MB85RC512T.h
+++ MB85RC512T.h
@@ -15,17 +15,26 @@ extern "C" {
 #include "main.h"
 
 #define MB85RC512T_VERSION "0.0.0"
-#define MB85RC512T_TEST 0
-#define MB85RC512T_TIMEOUT 100 // ms
+#define MB85RC512T_TIMEOUT (uint32_t)100 // ms
 #define MB85RC512T_INTERRUPT 0
+#define MB85RC512T_MAX_ADDRESS (uint32_t)65536 // bytes
+#define MB85RC512T_BUFFLEN 64
+#define MB85RC512T_WRITE_LEN (uint32_t)32 // must be smaller than bufflen
+#define MB85RC512T_PRINT 0
+
+#if MB85RC512T_PRINT == 1
+#define MB85RC512T_TIMEOUT_UART (uint32_t)100 // ms
+#endif
 
 typedef enum
 {
 	MB85RC512T_NONE = -1,
-	MB85RC512T_OK,
-	MB85RC512T_ERROR,
+	MB85RC512T_OK = 0,
+	MB85RC512T_ERROR_INIT,
+	MB85RC512T_ERROR_ADDRESS,
 	MB85RC512T_ERROR_TX,
-	MB85RC512T_ERROR_RX
+	MB85RC512T_ERROR_RX,
+	MB85RC512T_ERROR_UART,
 } MB85RC512T_State;
 
 typedef struct
@@ -33,19 +42,22 @@ typedef struct
 	I2C_HandleTypeDef *m_hi2c;
 	uint8_t m_address;
 	uint8_t m_init;
-	uint8_t m_data_rx[8];
-	uint8_t m_data_tx[8];
+	uint8_t m_data_rx[MB85RC512T_BUFFLEN];
+	uint8_t m_data_tx[MB85RC512T_BUFFLEN];
 } MB85RC512T;
 
 MB85RC512T_State MB85RC512T_init(MB85RC512T *device, I2C_HandleTypeDef *hi2c, const uint8_t address);
 MB85RC512T_State MB85RC512T_deinit(MB85RC512T *device);
 
-MB85RC512T_State MB85RC512T_write8(MB85RC512T *device, const uint8_t data);
-MB85RC512T_State MB85RC512T_read8(MB85RC512T *device, uint8_t *data);
+MB85RC512T_State MB85RC512T_write(MB85RC512T *device, const uint32_t address, const uint8_t *data, const size_t len);
+MB85RC512T_State MB85RC512T_read(MB85RC512T *device, const uint32_t address, uint8_t *data, const size_t len);
 
-#if MB85RC512T_TEST == 1
+MB85RC512T_State MB85RC512T_reset(MB85RC512T *device, const uint8_t value);
 
+#if MB85RC512T_PRINT == 1
+MB85RC512T_State MB85RC512T_print(MB85RC512T *device, UART_HandleTypeDef *huart);
 #endif
+
 
 #ifdef __cplusplus
 }
