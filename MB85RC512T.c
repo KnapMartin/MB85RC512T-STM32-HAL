72af46850b0b9d297dfe8b626cd52f47a58a74ea
KnapMartin <martin_knap@yahoo.com>
2025-02-18 10:16:28 +0100

    add doxygen comments

--- MB85RC512T.c
+++ MB85RC512T.c
@@ -12,6 +12,14 @@
 #include <stdio.h>
 #endif
 
+/**
+ * @brief Initialize device object.
+ * 
+ * @param self Device object pointer.
+ * @param hi2c HAL I2C handle.
+ * @param address Device I2C address.
+ * @return MB85RC512T_State 
+ */
 MB85RC512T_State MB85RC512T_init(struct MB85RC512T *self, I2C_HandleTypeDef *hi2c, const uint8_t address)
 {
 	if (address < 0x08 || address > 0x77)
@@ -27,6 +35,12 @@ MB85RC512T_State MB85RC512T_init(struct MB85RC512T *self, I2C_HandleTypeDef *hi2
 	return MB85RC512T_OK;
 }
 
+/**
+ * @brief Deinitialize device struct.
+ * 
+ * @param self Device object pointer.
+ * @return MB85RC512T_State 
+ */
 MB85RC512T_State MB85RC512T_deinit(struct MB85RC512T *self)
 {
 	self->m_address = 0;
@@ -36,6 +50,15 @@ MB85RC512T_State MB85RC512T_deinit(struct MB85RC512T *self)
 	return MB85RC512T_OK;
 }
 
+/**
+ * @brief Device write function. To write at selected address pass a buffer pointer and buffer length.
+ * 
+ * @param self Device object pointer.
+ * @param address Selected device address.
+ * @param data Data to be written.
+ * @param len Length of data to be written.
+ * @return MB85RC512T_State 
+ */
 MB85RC512T_State MB85RC512T_write(struct MB85RC512T *self, const uint32_t address, const uint8_t *data, const size_t len)
 {
 	if (!self->m_init) return MB85RC512T_ERROR_INIT;
@@ -66,6 +89,15 @@ MB85RC512T_State MB85RC512T_write(struct MB85RC512T *self, const uint32_t addres
 	return MB85RC512T_OK;
 }
 
+/**
+ * @brief Device read function. To read from selected address pass a buffer pointer and buffer length.
+ * 
+ * @param self Device object pointer.
+ * @param address Selected device address.
+ * @param data Outout buffer.
+ * @param len Length of data to be read.
+ * @return MB85RC512T_State 
+ */
 MB85RC512T_State MB85RC512T_read(struct MB85RC512T *self, const uint32_t address, uint8_t *data, const size_t len)
 {
 	if (!self->m_init) return MB85RC512T_ERROR_INIT;
@@ -97,6 +129,13 @@ MB85RC512T_State MB85RC512T_read(struct MB85RC512T *self, const uint32_t address
 	return MB85RC512T_OK;
 }
 
+/**
+ * @brief Reset device state to a selected value.
+ * 
+ * @param self Device object pointer.
+ * @param value Value to be set.
+ * @return MB85RC512T_State 
+ */
 MB85RC512T_State MB85RC512T_reset(struct MB85RC512T *self, const uint8_t value)
 {
     if (!self->m_init) return MB85RC512T_ERROR_INIT;
@@ -125,6 +164,13 @@ MB85RC512T_State MB85RC512T_reset(struct MB85RC512T *self, const uint8_t value)
 
 
 #if MB85RC512T_PRINT == 1
+/**
+ * @brief Print device state.
+ * 
+ * @param self Device object pointer.
+ * @param huart HAL UART handle pointer.
+ * @return MB85RC512T_State 
+ */
 MB85RC512T_State MB85RC512T_print(struct MB85RC512T *self, UART_HandleTypeDef *huart)
 {
 	if (!self->m_init) return MB85RC512T_ERROR_INIT;
